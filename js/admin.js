6f2deece688c827f9b3997becfad4c77d35f7d16
Francis Kiryowa <franciskiryowa68@gmail.com>
2018-11-05 00:57:37 -0500

    Feature:To allow a user to send their parcel order [Delivers #161704420]

--- js/admin.js
+++ js/admin.js
@@ -1,25 +1,59 @@
 // Get the modal
-var modal = document.getElementById('myModal');
+var sendersModal = document.getElementById("sendersModal");
+var receiversModal = document.getElementById("receiversModal");
+var updateOrderStatusModal = document.getElementById("updateOrderStatusModal");
+var updateLocationStatusModal = document.getElementById("updateLocationStatusModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var close1 = document.getElementsByClassName("close1")[0];
+var close2 = document.getElementsByClassName("close2")[0];
+var close3 = document.getElementsByClassName("close3")[0];
+var close4 = document.getElementsByClassName("close4")[0];
 
-function MakeOrder1(){
-    modal.style.display = "block";
+function SendersDetails() {
+    sendersModal.style.display = "block";
 }
 
+function ReceiversDetails() {
+    receiversModal.style.display = "block";
+}
+
+function updateOrderStatus() {
+    updateOrderStatusModal.style.display ="block";
+}
+
+function UpdateOrderLocation() {
+    updateLocationStatusModal.style.display = "block";
+}
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
+close1.onclick = function() {
+    sendersModal.style.display = "none";
+}
+
+close2.onclick = function() {
+    receiversModal.style.display = "none";
+}
+close3.onclick = function() {
+    updateOrderStatusModal.style.display = "none";
+}
+close4.onclick = function() {
+    updateLocationStatusModal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    if (event.target == sendersModal) {
+        sendersModal.style.display = "none";
+    }else if(event.target == receiversModal) {
+        receiversModal.style.display = "none";
     }
+    else if(event.target == updateOrderStatusModal) {
+        updateOrderStatusModal.style.display = "none";
+    }
+    else if(event.target == updateLocationStatusModal) {
+        updateLocationStatusModal.style.display = "none";
+    }
+
 }
--- js/sendit.js
+++ js/sendit.js
@@ -194,6 +194,82 @@ window.onclick = function(event) {
   }
 } 
 
+// Get the modal
+var modal = document.getElementById('myModal');
+var GoogleModal = document.getElementById("GoogleModal");
+
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+var close2  = document.getElementsByClassName("close2")[0];
+
+function MakeOrder1(){
+    modal.style.display = "block";
+}
+
+function GoogleMap() {
+  GoogleModal.style.display ="block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+close2.onclick = function() {
+    GoogleModal.style.display = "none";
+}
+
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }else if(event.target == GoogleModal) {
+        GoogleModal.style.display = "none";
+    }
+}
+
+// function to allow user to change destination
+function changeDestination() {
+    var destination = document.getElementById("destination");
+    var place = document.getElementById("place");
+    var newdestination = document.getElementById("newdestination");
+    var save_button_destination = document.getElementById("save_button_destination");
+    var cancel_buttton_destination =  document.getElementById("cancel_buttton_destination");
+    destination.style.display = "none";
+    place.style.display="none";
+    newdestination.style.display = "block";
+    save_button_destination.style.display = "block";
+    cancel_buttton_destination.style.display="block";
+    
+}
+
+
+/* When the user clicks on the button,toggle between hiding and showing the dropdown content */
+function viewNotifications() {
+    var view_notifications = document.getElementById("view_notifications");
+    view_notifications.style.display = "block";
+    document.getElementById("notifications_drop_down").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+if (!event.target.matches('.imagedropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+    var openDropdown = dropdowns[i];
+    if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+    }
+    }
+}
+}
+
 
 
 
