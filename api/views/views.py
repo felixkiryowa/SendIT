1bf7e9da3b96baa9665f5690ca43fbaffa89b6c8
felixkiryowa <franciskiryowa68@gmail.com>
2018-11-08 00:37:19 -0800

    Feature:To enable a user to get all orders created by a specific user [Delivers #161749143]

--- api/views/views.py
+++ api/views/views.py
@@ -3,6 +3,7 @@ This module handles view routes
 
 """
 from api.endpoints.orders import OrdersApi
+from api.endpoints.get_specific_user_orders import UserSpecificOrders
 
 class GetOrderApiUrls:
 
@@ -13,12 +14,17 @@ class GetOrderApiUrls:
     def get_api_urls(self, app):
         """function defining all the api routes """
         order_view = OrdersApi.as_view('order_api')
+        user_specific_orders_view = UserSpecificOrders.as_view('user_orders')
         app.add_url_rule(
             '/api/v1/parcels', defaults={'order_id': None},
             view_func=order_view, methods=['GET',]
         )
         app.add_url_rule(
             '/api/v1/parcels/<int:order_id>',
-             view_func=order_view, methods=['GET', 'PUT', 'DELETE']
+             view_func=order_view, methods=['GET',]
         )
         app.add_url_rule('/api/v1/parcels', view_func=order_view, methods=['POST',])
+        app.add_url_rule(
+            '/api/v1/users/<int:user_id>/parcels',
+             view_func=user_specific_orders_view, methods=['GET',]
+        )
