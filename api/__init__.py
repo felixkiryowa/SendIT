4d6e9f24a4c8e58d1c16b44a0c32b928d502de92
felixkiryowa <franciskiryowa68@gmail.com>
2018-11-20 08:24:02 -0800

    Add heroku configurations to host on heroku

--- api/__init__.py
+++ api/__init__.py
@@ -1,4 +1,5 @@
 import psycopg2
+import os
 from flask import Flask
 # import configurations
 # from api.config  import app_config
@@ -7,7 +8,10 @@ secret_key = 'thisisasceretkey'
 
 # creating an object of GetOrderApiUrls
 # conn = psycopg2.connect(database="SendIT",user="postgres",password="atagenda1@",host="localhost",port="5432")
-conn = psycopg2.connect(database="testdb")
+if os.getenv('HEROKU_ENVIRONMENT') == 'heroku':
+    conn = psycopg2.connect(database="d7to87pvcitck8",user="cydhaaqyvycwlm",password="7b0226b6a6d1acdde593d5c6b628ebfc3b4a6f6439dd2e89c72ae9c96dc45ee6",host="ec2-23-23-101-25.compute-1.amazonaws.com",port="5432")
+else:
+    conn = psycopg2.connect(database="testdb")
 
 from api.views.views import GetOrderApiUrls
 GetOrderApiUrls.get_api_urls(app)
