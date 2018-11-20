343a27dc531086e621ee97a64d1b58f551b09734
felixkiryowa <franciskiryowa68@gmail.com>
2018-11-20 03:37:28 -0800

    Modify travis file

--- api/__init__.py
+++ api/__init__.py
@@ -1,4 +1,5 @@
 import psycopg2
+import os 
 from flask import Flask
 # import configurations
 # from api.config  import app_config
@@ -6,7 +7,9 @@ app =  Flask(__name__)
 secret_key = 'thisisasceretkey'
 
 # creating an object of GetOrderApiUrls
-conn = psycopg2.connect(database="SendIT",user="postgres",password="atagenda1@")
+if os.getenv('TESTING_DATABASE') == 'test_db':
+    conn = psycopg2.connect(database="test_db")
+conn = psycopg2.connect(database="SendIT",user="postgres",password="atagenda1@",host="localhost")
 # conn = psycopg2.connect(database="d7to87pvcitck8",user="cydhaaqyvycwlm",password="7b0226b6a6d1acdde593d5c6b628ebfc3b4a6f6439dd2e89c72ae9c96dc45ee6",host="ec2-23-23-101-25.compute-1.amazonaws.com",port="5432")
 
 # conn = psycopg2.connect(database="SendIT")
