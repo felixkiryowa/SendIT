3ac21ea748fedd8e1d35fa458da6ecc9429cab49
felixkiryowa <franciskiryowa68@gmail.com>
2018-11-22 03:19:44 -0800

    Add databasne connection to heroku

--- api/__init__.py
+++ api/__init__.py
@@ -8,12 +8,14 @@ secret_key = 'thisisasceretkey'
 
 # creating an object of GetOrderApiUrls
 # connection = psycopg2.connect(database="SendIT",user="postgres",password="atagenda1@",host="localhost",port="5432")
-# if os.getenv('HEROKU_ENVIRONMENT') == 'heroku':
-#     connection = psycopg2.connect(database="d7to87pvcitck8",user="cydhaaqyvycwlm",password="7b0226b6a6d1acdde593d5c6b628ebfc3b4a6f6439dd2e89c72ae9c96dc45ee6",host="ec2-23-23-101-25.compute-1.amazonaws.com",port="5432")
-# else:
+if os.getenv('HEROKU_ENVIRONMENT') == 'heroku':
+    connection = psycopg2.connect(database="d7to87pvcitck8",user="cydhaaqyvycwlm",password="7b0226b6a6d1acdde593d5c6b628ebfc3b4a6f6439dd2e89c72ae9c96dc45ee6",host="ec2-23-23-101-25.compute-1.amazonaws.com",port="5432")
+else:
+    connection = psycopg2.connect(database="testdb")
+
 # connection = psycopg2.connect(database="SendIT",user="postgres",password="admin",host="localhost",port="5555")
 
-connection = psycopg2.connect(database="testdb")
+
 
 from api.routes.views import GetOrderApiUrls
 GetOrderApiUrls.get_api_urls(app)
